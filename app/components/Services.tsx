d44fcfdf63b3f78b5ac7da5bdfd68bb91f4e5501
Charan-teja1234 <adimulacharan@gmail.com>
2026-01-16 17:47:39 +0530

    Update Services.tsx

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -44,7 +44,7 @@ export default function Services() {
   ]
 
   return (
-    <section className="py-16 md:py-24 bg-white">
+    <section id="services" className="py-16 md:py-24 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div 
           ref={titleRef}
